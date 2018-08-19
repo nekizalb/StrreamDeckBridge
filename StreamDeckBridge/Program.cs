5dc0529c3991e3dd6a59e009b5feb023b7147302
Tyler <nekizalb@gmail.com>
2018-08-19 17:13:29 -0600

    initial setup

---
+++ StreamDeckBridge/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace StreamDeckBridge
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Actual window service code=
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                    new StreamDeckBridge()
+            };
+
+            if (Environment.UserInteractive)
+            {
+                RunInteractive(ServicesToRun);
+            }
+            else
+            {
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+
+        //found on https://stackoverflow.com/questions/125964/easier-way-to-debug-a-windows-service/10838170#10838170
+        static void RunInteractive(ServiceBase[] servicesToRun)
+        {
+            Console.WriteLine("Services running in interactive mode.");
+            Console.WriteLine();
+
+            MethodInfo onStartMethod = typeof(ServiceBase).GetMethod("OnStart",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Starting {0}...", service.ServiceName);
+                onStartMethod.Invoke(service, new object[] { new string[] { } });
+                Console.Write("Started");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(
+                "Press any key to stop the services and end the process...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            MethodInfo onStopMethod = typeof(ServiceBase).GetMethod("OnStop",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Stopping {0}...", service.ServiceName);
+                onStopMethod.Invoke(service, null);
+                Console.WriteLine("Stopped");
+            }
+
+            Console.WriteLine("All services stopped.");
+            // Keep the console alive for a second to allow the user to see the message.
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ StreamDeckBridge/StreamDeckBridge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StreamDeckBridge
+{
+    public partial class StreamDeckBridge : ServiceBase
+    {
+        public StreamDeckBridge()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            MessageBox.Show("hi!");
+        }
+
+        protected override void OnStop()
+        {
+            MessageBox.Show("bye!");
+        }
+    }
+}
